08c4a0d552441cc5094e4dc3a6ec65291c08fd46
Apple <apple@applesmacbook6.wireless.rit.edu>
2016-01-27 22:06:52 -0500

    Initial commit.

---
+++ Scavenger Hunt/ListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ListViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Apple on 1/27/16.
+//  Copyright © 2016 Nick Mosher. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ListViewController : UITableViewController {
+    
+    var itemsList = [
+        ScavengerHuntItem(name: "Cat"),
+        ScavengerHuntItem(name: "Bird"),
+        ScavengerHuntItem(name: "Brick")
+    ]
+    
+    @IBAction func unwindToList(segue: UIStoryboardSegue) {
+        
+        if segue.identifier == "doneItem" {
+            let addVC = segue.sourceViewController as! AddViewController
+            if let newItem = addVC.newItem {
+                itemsList += [newItem]
+                let indexPath = NSIndexPath(forRow: itemsList.count - 1, inSection: 0)
+                tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+        return itemsList.count;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = itemsList[indexPath.row].name
+            
+        return cell
+    }
+}
